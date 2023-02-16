e7988c163e77fbec9cf3f56f301220071d5da723
KacperMurawski <124902244+KacperMurawski@users.noreply.github.com>
2023-02-16 16:22:42 +0100

    16.02

---
+++ kacper.py
@@ -0,0 +1,24 @@
+#  print drukowanie na ekranie
+
+
+# input pytanie usera o cos tam
+
+x = "xddd"
+#print(len(x)) # liczy znaki w stringu i je wypluwa
+#print(x[0]) #pisze znak z stringa na pozycji 0 (czyli s)
+#print(x[0:3]) #to samo co na gorze, tylko, ze do 3 pozycji
+#print(x[-1]) #daje znak liczac pozycje od konca (pomija 0 czyli wartosc -1)
+#print(x[:]) #dziala tak samo jak print(x)
+#print(x[::2]) #daje co drugi znak
+#[x,y,z] -> x - poczatek y - koniec z - jakis krok
+#imie = "kacper"
+#nazwisko = "murawski"
+#calosc = imie + nazwisko
+#print(imie*10 + nazwisko) #pomnozenie jednego slowa
+#print(f"Witaj {imie} w pytonie") #f daje znac pythonowi ze {imie} to zmienna
+#print(type(imie)) #daje nam rodzaj zmiennej imie\
+#x = input("Podaj swój wiek :")
+#print(int(x) +2)
+james_bond = 7
+#cel = 007 -> funkcja zfill()
+print(str(james_bond).zfill(3))
