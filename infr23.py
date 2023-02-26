4ebab4c8d26697337886539a357cbdea63138134
KacperMurawski <124902244+KacperMurawski@users.noreply.github.com>
2023-02-26 13:21:28 +0100

    praca z lekcji 24.02

---
+++ infr23.py
@@ -0,0 +1,52 @@
+# course = " xdd" # to jest  string
+# print(course)
+# print(course.upper()) #robi slowo z caps lockiem
+# print(course.lower())
+# print(course.title()) #liczba duzych liter w zaleznosci od spacji przy zmiennej course
+# print(course.lstrip()) #wycina cos z danej strony l/rstrip
+# print(course.find("x")) #wyszukuje pozycje danego znaku
+# print(course.replace("d", "x"))  #zamienia literke x na y
+# print("d" in course)#sprawdza czy dana rzecz znajduje sie w stringu
+# print("z" not in course)#sprawdza czy cos nie znajduje sie w stringu
+#
+# print(10 / 3)#dzielenie
+# print(10 // 3) #to samo bez ułamka
+# print(10 % 3)#reszta dzielenia z 10/3
+# print(10 ** 3) #do potegi x
+#
+#
+# import math
+#
+# print(round(2.9))#zaokraglenie
+# print(abs(-2.9))#wartosc bezwzgledna
+#
+# print(math.ceil(2.2))#podbijanie do wyzszej liczby calkowitej
+# print(math.floor(2.2))#schodzenie do nizszej liczby calkowitej
+#
+#
+#
+# x = input("podaj cos: ")
+# y = int(x) + 1
+# print(f"x {x}, y: {y}")
+
+# print(ord("b"))
+# print(ord("a"))
+
+#temperature = 15
+#if temperature > 15:
+    #print("Jest cieplo")
+    #print("Pij wode")
+#else:
+    #print("zimno jest")
+    #print("ubierz sie")
+
+# age = input("podaj swoj wiek: ")
+# if int(age)>= 18:
+#     print("odpowiedni")
+# else:
+#     print("nieodpowiedni")
+
+#for zmienna in range(4, 10, 2):
+    #print(zmienna)
+    
+
