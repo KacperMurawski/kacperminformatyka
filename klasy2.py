8e7481a91755581422f5ee58bc59d3c49d0c29ab
KacperMurawski <124902244+KacperMurawski@users.noreply.github.com>
2023-11-23 12:17:05 +0100

    Add files via upload

---
+++ klasy2.py
@@ -0,0 +1,41 @@
+#class Point: #klase piszemy zawsze z duzej litery
+   # def __init__(self, x, y): #od razu uruchamia czynnosc
+        #self.x = x
+        #self.y = y
+
+    #def draw(self):
+        #print(f"Point ({self.x}, {self.y})")
+#print(type(point)) #daje nam klase instancji
+#print(isinstance(print, Point)) #sprawdza, czy podana zmienna ma taka klase
+
+#point.draw()
+
+#another = Point(3,4)
+#another.draw()
+
+class Book:
+    def __init__(self):
+        self.book=book
+        self.title = "zycie jasia"
+        self.author = "jasiu"
+        self.year = 2020
+        self.is_avaiable= True
+        
+
+
+
+
+
+
+
+
+
+
+class Library:
+    def __init__(self, ksiazka):
+    def add_book(self):
+    def display_books(self):
+    def borrow_book(self, title):
+    def return_book(self, title):
+
+
